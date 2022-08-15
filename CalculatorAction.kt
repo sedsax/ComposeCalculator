961fc51471dab611d5ee97fc459c76faaa491586
Sedanur Savaş <44877350+sedsax@users.noreply.github.com>
2022-08-15 20:54:56 +0300

    Create CalculatorAction.kt

---
+++ CalculatorAction.kt
@@ -0,0 +1,10 @@
+package com.ankauniss.composecalculator
+
+sealed class CalculatorAction {
+    data class Number(val number: Int): CalculatorAction()
+    object Clear: CalculatorAction()
+    object Delete: CalculatorAction()
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+    object Calculate: CalculatorAction()
+    object Decimal: CalculatorAction()
+}
